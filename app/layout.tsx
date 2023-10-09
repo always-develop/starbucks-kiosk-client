687a4499a55aec9863056eded6e038d61ea0782f
appleSimple <jy_4148@naver.com>
2023-10-10 00:07:40 +0900

    feat : make common header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './globals.css';
+import { AiOutlineHome } from 'react-icons/ai';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header className="w-full flex items-center justify-center relative pt-5 pb-5 border-b-[1px]">
+          <div className="flex items-center gap-5">
+            <img className="h-20 w-20" src="/assets/icons/logo.png" alt="로고" />
+            <h1 className="text-3xl font-semibold text-gray-900">Starbucks</h1>
+          </div>
+          <div className="absolute right-0 mr-8">
+            <AiOutlineHome className="text-3xl" />
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
