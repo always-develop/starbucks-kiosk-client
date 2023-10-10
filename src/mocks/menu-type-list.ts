93fc5f4c9afc6c883b28094887c25ba4bbe2fa5f
appleSimple <jy_4148@naver.com>
2023-10-10 21:50:42 +0900

    feat : menuTypeList 작성

---
+++ src/mocks/menu-type-list.ts
@@ -0,0 +1,74 @@
+import { MenuType } from 'src/types/menuTypeItem';
+
+export const menuTypeList: MenuType[] = [
+	{
+		id: 0,
+		name: {
+			ko: '콜드 브루 커피',
+			en: 'cold-brew'
+		},
+	},
+	{
+		id: 1,
+		name: {
+			ko: '브루드',
+			en: 'brewed'
+		},
+	},
+	{
+		id: 2,
+		name: {
+			ko: '에스프레소',
+			en: 'espresso'
+		},
+	},
+	{
+		id: 3,
+		name: {
+			ko: '프라푸치노',
+			en: 'frappuccino'
+		},
+	},
+	{
+		id: 4,
+		name: {
+			ko: '블렌디드',
+			en: 'blended'
+		},
+	},
+	{
+		id: 5,
+		name: {
+			ko: '스타벅스 리프레셔',
+			en: 'refreshers'
+		},
+	},
+	{
+		id: 6,
+		name: {
+			ko: '스타벅스 피지오',
+			en: 'fizzio'
+		},
+	},
+	{
+		id: 7,
+		name: {
+			ko: '티(티바나)',
+			en: 'tea'
+		},
+	},
+	{
+		id: 8,
+		name: {
+			ko: '기타 제조 음료',
+			en: 'etc'
+		},
+	},
+	{
+		id: 9,
+		name: {
+			ko: '스타벅스 주스(병음료)',
+			en: 'juice'
+		},
+	},
+];
